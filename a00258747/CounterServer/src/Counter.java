b2a6a6353af71ca355beec3a55fbaf444780420a
Suriya <suriyaswami2@gmail.com>
2018-11-05 10:37:18 +0000

    Update Counter.java

--- a00258747/CounterServer/src/Counter.java
+++ a00258747/CounterServer/src/Counter.java
@@ -6,5 +6,6 @@ interface Counter extends Remote {
 public void increment() throws RemoteException;
 public void decrement() throws RemoteException;
 public int read_value() throws RemoteException;
+public void reset_value() throws RemoteException;
 }
 
